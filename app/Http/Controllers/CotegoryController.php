a4a1d65e95089c98301a68ec214c120dd4384e55
bobbyza797 <bobby.anon79@gmail.com>
2021-01-15 00:48:28 +0700

    category model migration controller add

---
+++ app/Http/Controllers/CotegoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\cotegory;
+use Illuminate\Http\Request;
+
+class CotegoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\cotegory  $cotegory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cotegory $cotegory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\cotegory  $cotegory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cotegory $cotegory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\cotegory  $cotegory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cotegory $cotegory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\cotegory  $cotegory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cotegory $cotegory)
+    {
+        //
+    }
+}
